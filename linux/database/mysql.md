236c7d0b6cc29ce35ad73817c95485755c940ded
MaYingJie <mayingjie005@gmail.com>
2019-02-12 18:39:32 +0800

    mysql总结1

---
+++ linux/database/mysql.md
@@ -0,0 +1,7 @@
+# FLOAT 数据精度
+
+# timestamp defalut值
+
+# 所有的自增id 采用BIGINT
+
+# 数据表的可扩展性
--- linux/namespace/namespace.c
+++ linux/namespace/namespace.c
@@ -10,13 +10,14 @@
 static char container_stack[STACK_SIZE];                                        
 char* const container_args[] = {                                                
    "/bin/bash",                                                                 
-   "echo hello"                                                                 
+   "hostname"                                                                 
 };                                                                              
                                                                                 
 // 容器进程运行的程序主函数                                                     
 int container_main(void *args)                                                  
 {                                                                               
-   printf("在容器进程中！\n");                                                  
+   printf("在容器进程中！\n");
+sethostname("container",10);   
    execv(container_args[0], container_args); // 执行/bin/bash   return 1;       
 }                                                                               
                                                                                 
@@ -24,7 +25,7 @@ int main(int args, char *argv[])
 {                                                                               
    printf("程序开始\n");                                                        
    // clone 容器进程                                                            
-   int container_pid = clone(container_main, container_stack + STACK_SIZE, SIGCHLD, NULL);
+   int container_pid = clone(container_main, container_stack + STACK_SIZE, CLONE_NEWUTS|SIGCHLD, NULL);
    // 等待容器进程结束                                                          
    waitpid(container_pid, NULL, 0);                                             
    return 0;                                                                    
